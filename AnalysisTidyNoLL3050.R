98352a1ba934b8fa294517a03c50bf629bc62e65
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-06-14 17:32:16 -0400

    Initial commit for fit result analysis

---
+++ AnalysisTidyNoLL3050.R
@@ -0,0 +1,50 @@
+#library(dplyr)
+
+# Read in the data and clean up names
+setwd("~/Analysis/lambda/AliAnalysisLambda/Fitting/FemtoFitting/FitResults")
+data <- read.csv("FitResultsTidyNoLL3050.csv")
+cleanNames <- gsub(".", "", colnames(data), fixed = TRUE)
+colnames(data) <- cleanNames
+summary(data)
+
+# Remove the LLAA 3050 columns
+ll3050Cols <- grepl("LLAA3050", colnames(data))
+data <- data[!ll3050Cols]
+
+# Separate the value columns from the error columns
+errColIndices <- grepl("Err",colnames(data))
+errors <- data[errColIndices]
+values <- data[!errColIndices]
+
+
+# Find the mean, RMS, min, and max of the data
+RMS <- function(vals) {
+    # Remove na
+    clean <- na.omit(vals)
+    quad <- clean^2
+    quadMean <- mean(quad)
+    sqrt(quadMean)
+}
+means <- colMeans(values, na.rm = TRUE)
+rms <- sapply(values, RMS)
+max <- sapply(values, max, na.rm=TRUE)
+min <- sapply(values, min, na.rm=TRUE)
+
+# Calculate the mean error and the rms error
+errMeans <- colMeans(errors, na.rm = TRUE)
+errRMS <- sapply(errors, RMS)
+
+# Combine everything together
+resultsAll <- data.frame(Type = colnames(values), Mean = as.numeric(means), 
+                      RMS = rms, Max = max, Min = min, 
+                      MeanError = errMeans, RMSError = errRMS)
+resultsAll$Sigma <- sqrt(resultsAll$RMS^2 - resultsAll$Mean^2)
+
+# Most of the time, we won't really care about the normalization
+# or the bkg coefficients. Let's just take radius, Ref0, Imf0, and d0.
+results <- resultsAll[grep("Rad|ReF0|ImF0LA|D0", rownames(resultsAll)), ]
+
+write.csv(results, file = "FitTable.csv")
+
+
+4
---
+++ PlotResults.R
@@ -0,0 +1,83 @@
+results <- read.csv("FitTable.csv")
+
+
+library(ggplot2)
+gMeans <- ggplot(results, aes(x=Type, y=Mean))
+gMeans <- gMeans+geom_point(stat="identity",position=position_dodge(width = 1))
+limits <- aes(ymax = Max, ymin = Min)
+
+# Add on the systematic error bars (Max/Min values)
+gMeans <- gMeans + geom_errorbar(limits)
+
+# Also add in std deviation error bar
+gMeans <- gMeans +geom_errorbar(aes(ymax = Mean + Sigma, ymin = Mean - Sigma),
+                                position = position_dodge(width = 1),
+                                width = 1,
+                                color = "purple")
+
+# Add in statistical error bars (mean error)
+# gMeans <- gMeans + geom_errorbar(aes(ymax = Mean + MeanError, ymin = Mean - MeanError),
+#                                 position=position_dodge(width = 1),
+#                                 width = 0.5,
+#                                 color = "red")
+
+# Add in statistical error bars (rms error)
+gMeans <- gMeans + geom_errorbar(aes(ymax = Mean + RMSError, ymin = Mean - RMSError),
+                                 position=position_dodge(width = 1),
+                                 width = 0.5,
+                                 color = "blue")
+
+
+
+gMeans <- gMeans + theme(text = element_text(size=20),
+                         axis.text.x = element_text(angle=90, vjust=1))
+
+gMeans
+
+#gDot <- ggplot(dataTrim, aes(x=LA010)) + geom_dotplot()
+#gDot <- gDot + aes(x=LA1030)
+#gDot
+
+
+# Look just at d0 for LLAA
+d0 <- data[!is.na(data$D0LLAA010), c("D0LLAA010", "D0LLAA010Err")]
+gd0 <- ggplot(d0, aes(y=D0LLAA010, x=rownames(d0)))
+gd0 <- gd0 + geom_point(stat="identity", position = position_dodge(width=1))
+gd0Limits <- aes(ymax = D0LLAA010 + D0LLAA010Err, ymin = D0LLAA010 - D0LLAA010Err)
+gd0 <- gd0 + geom_errorbar(gd0Limits)
+gd0
+
+ReF0 <- data[!is.na(data$ReF0LLAA010), c("ReF0LLAA010", "ReF0LLAA010Err")]
+gReF0 <- ggplot(ReF0, aes(y=ReF0LLAA010, x=rownames(ReF0)))
+gReF0 <- gReF0 + geom_point(stat="identity", position = position_dodge(width=1))
+gReF0Limits <- aes(ymax = ReF0LLAA010 + ReF0LLAA010Err, ymin = ReF0LLAA010 - ReF0LLAA010Err)
+gReF0 <- gReF0 + geom_errorbar(gReF0Limits)
+gReF0
+
+ReF0 <- data[!is.na(data$ReF0LA010), c("ReF0LA010", "ReF0LA010Err")]
+gReF0 <- ggplot(ReF0, aes(y=ReF0LA010, x=rownames(ReF0)))
+gReF0 <- gReF0 + geom_point(stat="identity", position = position_dodge(width=1))
+gReF0Limits <- aes(ymax = ReF0LA010 + ReF0LA010Err, ymin = ReF0LA010 - ReF0LA010Err)
+gReF0 <- gReF0 + geom_errorbar(gReF0Limits)
+gReF0
+
+Radius <- data[!is.na(data$RadiusLLAA010), c("RadiusLLAA010", "RadiusLLAA010Err")]
+gRadius <- ggplot(Radius, aes(y=RadiusLLAA010, x=rownames(Radius)))
+gRadius <- gRadius + geom_point(stat="identity", position = position_dodge(width=1))
+gRadiusLimits <- aes(ymax = RadiusLLAA010 + RadiusLLAA010Err, ymin = RadiusLLAA010 - RadiusLLAA010Err)
+gRadius <- gRadius + geom_errorbar(gRadiusLimits)
+gRadius
+
+Norm <- data[!is.na(data$NormLLAA010), c("NormLLAA010", "NormLLAA010Err")]
+gNorm <- ggplot(Norm, aes(y=NormLLAA010, x=rownames(Norm)))
+gNorm <- gNorm + geom_point(stat="identity", position = position_dodge(width=1))
+gNormLimits <- aes(ymax = NormLLAA010 + NormLLAA010Err, ymin = NormLLAA010 - NormLLAA010Err)
+gNorm <- gNorm + geom_errorbar(gNormLimits)
+gNorm
+
+d0 <- data[!is.na(data$D0LA010), c("D0LA010", "D0LA010Err")]
+gd0 <- ggplot(d0, aes(y=D0LA010, x=rownames(d0)))
+gd0 <- gd0 + geom_point(stat="identity", position = position_dodge(width=1))
+gd0Limits <- aes(ymax = D0LA010 + D0LA010Err, ymin = D0LA010 - D0LA010Err)
+gd0 <- gd0 + geom_errorbar(gd0Limits)
+gd0
