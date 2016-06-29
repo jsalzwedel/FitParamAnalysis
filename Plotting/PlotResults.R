afd4b84defbb4a0942bbb9bb1400161a95a8ec97
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-06-29 17:00:23 -0400

    WIP function to plot all the measurements of a parameter

--- Plotting/PlotResults.R
+++ Plotting/PlotResults.R
@@ -1,4 +1,4 @@
-results <- read.csv("FitTableGlobalOnly.csv")
+results <- read.csv("FitTableSepFit.csv")
 
 
 library(ggplot2)
@@ -7,8 +7,8 @@ gMeans <- gMeans+geom_point(stat="identity",position=position_dodge(width = 1))
 limits <- aes(ymax = Max, ymin = Min)
 
 # Add on the systematic error bars (Max/Min values)
-gMeans <- gMeans + geom_errorbar(limits,
-                                 color = "red")
+# gMeans <- gMeans + geom_errorbar(limits,
+#                                  color = "red")
 
 # Also add in std deviation error bar
 gMeans <- gMeans +geom_errorbar(aes(ymax = Mean + Sigma, ymin = Mean - Sigma),
@@ -40,24 +40,41 @@ gMeans
 #gDot <- gDot + aes(x=LA1030)
 #gDot
 
+PlotMeasurements <- function(data, valColName) {
+    
+    # Get the columns indices associated with valColName and its error column
+    errColName <- paste(valColName,"Err", sep = "")
+    valColIndex <- which(colnames(data) == valColName)
+    errColIndex <- which(colnames(data) == errColName)
+    
+    # Subset the data to take just the non-NA rows of those two columns
+    dataset <- data[!is.na(data[,valColIndex]), c(valColName, errColName)]
+    
+    # Setup the ggplot of the data
+    g <- ggplot(dataset, aes(y=valColName, x=as.numeric(rownames(dataset))))
+    g <- g + geom_point(stat="identity", position = position_dodge(width=1))    
+    gLimits <- aes(ymax = valColName + errColName, ymin = valColName - errColName)
+    g <- g + geom_errorbar(gLimits)
+    g
+}
 
 # Look just at d0 for LLAA
 d0 <- data[!is.na(data$D0LLAA010), c("D0LLAA010", "D0LLAA010Err")]
-gd0 <- ggplot(d0, aes(y=D0LLAA010, x=rownames(d0)))
+gd0 <- ggplot(d0, aes(y=D0LLAA010, x=as.numeric(rownames(d0))))
 gd0 <- gd0 + geom_point(stat="identity", position = position_dodge(width=1))
 gd0Limits <- aes(ymax = D0LLAA010 + D0LLAA010Err, ymin = D0LLAA010 - D0LLAA010Err)
 gd0 <- gd0 + geom_errorbar(gd0Limits)
 gd0
 
 ReF0 <- data[!is.na(data$ReF0LLAA010), c("ReF0LLAA010", "ReF0LLAA010Err")]
-gReF0 <- ggplot(ReF0, aes(y=ReF0LLAA010, x=rownames(ReF0)))
+gReF0 <- ggplot(ReF0, aes(y=ReF0LLAA010, x=as.numeric(rownames(ReF0))))
 gReF0 <- gReF0 + geom_point(stat="identity", position = position_dodge(width=1))
 gReF0Limits <- aes(ymax = ReF0LLAA010 + ReF0LLAA010Err, ymin = ReF0LLAA010 - ReF0LLAA010Err)
 gReF0 <- gReF0 + geom_errorbar(gReF0Limits)
 gReF0
 
 ReF0 <- data[!is.na(data$ReF0LA010), c("ReF0LA010", "ReF0LA010Err")]
-gReF0 <- ggplot(ReF0, aes(y=ReF0LA010, x=rownames(ReF0)))
+gReF0 <- ggplot(ReF0, aes(y=ReF0LA010, x=as.numeric(rownames(ReF0))))
 gReF0 <- gReF0 + geom_point(stat="identity", position = position_dodge(width=1))
 gReF0Limits <- aes(ymax = ReF0LA010 + ReF0LA010Err, ymin = ReF0LA010 - ReF0LA010Err)
 gReF0 <- gReF0 + geom_errorbar(gReF0Limits)
