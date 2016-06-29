6f6d7b206c1dc54ceef903af7ef53f1581791ae5
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-06-29 11:55:26 -0400

    Moved analysis code from Cleaning/Clean.R into Analysis/Analyze.R Changed working dir in Clean.R

--- Cleaning/Clean.R
+++ Cleaning/Clean.R
@@ -1,5 +1,5 @@
 # Read in the data and clean up names
-setwd("~/Analysis/lambda/AliAnalysisLambda/Fitting/FemtoFitting/FitResults/RAnalysis/Analysis")
+setwd("~/Analysis/lambda/AliAnalysisLambda/Fitting/FemtoFitting/FitResults/RAnalysis/Cleaning")
 dataRaw <- read.csv("../FitTable-2016-06-28-CFWeightCombine.csv")
 cleanNames <- gsub(".", "", colnames(dataRaw), fixed = TRUE)
 colnames(dataRaw) <- cleanNames
@@ -29,39 +29,3 @@ dataSep <- data[!rowGlobal,]
 # Write the cleaned up data
 write.csv(dataGlobal, file = "CleanGlobalFits.csv")
 write.csv(dataSep, file = "CleanSepFits.csv")
-
-# Separate the value columns from the error columns
-errColIndices <- grepl("Err",colnames(data))
-errors <- data[errColIndices]
-values <- data[!errColIndices]
-
-
-# Find the mean, RMS, min, and max of the data
-RMS <- function(vals) {
-    # Remove na
-    clean <- na.omit(vals)
-    quad <- clean^2
-    quadMean <- mean(quad)
-    sqrt(quadMean)
-}
-means <- colMeans(values, na.rm = TRUE)
-rms <- sapply(values, RMS)
-max <- sapply(values, max, na.rm=TRUE)
-min <- sapply(values, min, na.rm=TRUE)
-
-# Calculate the mean error and the rms error
-errMeans <- colMeans(errors, na.rm = TRUE)
-errRMS <- sapply(errors, RMS)
-
-# Combine everything together
-resultsAll <- data.frame(Type = colnames(values), Mean = as.numeric(means), 
-                      RMS = rms, Max = max, Min = min, 
-                      MeanError = errMeans, RMSError = errRMS)
-resultsAll$Sigma <- sqrt(resultsAll$RMS^2 - resultsAll$Mean^2)
-
-# Most of the time, we won't really care about the normalization
-# or the bkg coefficients. Let's just take radius, Ref0, Imf0, and d0.
-results <- resultsAll[grep("Rad|ReF0|ImF0LA|D0", rownames(resultsAll)), ]
-
-write.csv(results, file = "FitTableGlobalOnly.csv")
-
